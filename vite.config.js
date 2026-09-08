b0852fb03623474e52ea0eeba1a9dca991c4f5e7
G Sudharsan <gsudharsan2701@gmail.com>
2026-09-08 15:07:20 +0530

    Add GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import { defineConfig } from 'vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // GitHub Pages repository path
+  base: '/ags-garden-city/',
+
   server: {
     port: 5173,
     strictPort: true
